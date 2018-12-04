221dbd6a84daea801803782c2a4ea0eb8d93f7ec
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2018-12-04 16:14:32 +0100

    ajout date+heure MAJ data.txt

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!doctype html>
-
+<?php
+$pageTemp = file_get_contents('data.txt');
+$tuff = json_decode($pageTemp);
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_TIME, 'fr_FR.utf8','fra');
+?>
 <html>
 	<head>
 		<title>Thermomètre</title>
@@ -11,10 +16,15 @@
 	<body>
 		<h1>Température</h1>
 		
+<!--faire varier le trait rouge en fonction de la temperature
+mettre à jour votre dépôt sur GitHub-->
+		<p>Il fait <?php echo $tuff->temperature; ?>° avec <?php echo $tuff->humidite ?>% d'humidité.</p>
+		
+		<p>Dernière mise à jour : <?php echo strftime("%A %d %B %Y à %H:%M:%S", filemtime('data.txt')); ?>.</p>
+		
 		<div id="thermometer">
 			<div id="bargraph"></div>
 		</div>
-
 
 	</body>
 </html>
