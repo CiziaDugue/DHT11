0e6bc16aaf46e968a2baf242e9e780be3e1d6698
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2018-12-04 15:42:46 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>Page Title</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1.0">
+		<link href="main.css" type="text/css" rel="stylesheet">
+	</head>
+
+	<body>
+		<h1>Température</h1>
+		
+		<div id="thermometer">
+			<div id="bargraph"></div>
+		</div>
+		
+	<?php
+	
+	echo 'hello world';
+	
+	
+	
+	?>
+
+	</body>
+</html>
