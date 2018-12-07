4b17ce09b9d3d6b520b289febfd170a4c4fb1a30
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2018-12-07 13:48:52 +0100

    comments+variables

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!doctype html>
 <?php
+//Récupération de l'objet json
+$
 $pageTemp = file_get_contents('data.txt');
-$tuff = json_decode($pageTemp);
+$data = json_decode($pageTemp);
+
+//Configuration du format de la date de maj
 date_default_timezone_set('Europe/Paris');
 setlocale(LC_TIME, 'fr_FR.utf8','fra');
+$majTime = strftime("%A %d %B %Y à %H:%M:%S", filemtime('data.txt'));
+
+//Conversion degré / pixels
+$bargraphHeight = 161 + $data->temperature * 4;
+$bargraphTop = 315 - $data->temperature * 4;
 ?>
 <html>
 	<head>
@@ -16,14 +25,14 @@ setlocale(LC_TIME, 'fr_FR.utf8','fra');
 	<body>
 		<h1>Température</h1>
 		
-		<p>Il fait <?php echo $tuff->temperature; ?>° avec <?php echo $tuff->humidite ?>% d'humidité.</p>
+		<p>Il fait <?php echo $data->temperature; ?>°C avec <?php echo $data->humidite ?>% d'humidité.</p>
 		
-		<p>Dernière mise à jour : <?php echo strftime("%A %d %B %Y à %H:%M:%S", filemtime('data.txt')); ?>.</p>
+		<p>Dernière mise à jour : <?php echo $majTime; ?>.</p>
 		
 		<div id="thermometer">
 			<div id="bargraph"></div>
 		</div>
 
-		<?php echo '<style>#bargraph{height:' . (161 + $tuff->temperature * 4) .'px;top:' . (315 - $tuff->temperature * 4) . 'px;}</style>'; ?>
+		<?php echo '<style>#bargraph{height:' . $bargraphHeight .'px;top:' . $bargraphTop . 'px;}</style>'; ?>
 	</body>
 </html>
