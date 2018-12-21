0bc286988f77c65521dabca0e657e09e3ee3e2c0
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2018-12-21 14:37:54 +0100

    pdo_step3

--- store_temp.php
+++ store_temp.php
@@ -1,57 +1,68 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PDO 1 : Connexion</title>
 
-//Paramètre
-$filename = 'data.txt';	
+        
+    </head>
+    <body>
+		<?php
 
-//Recupération des données json
-$data_json = file_get_contents('php://input');
+		//Paramètre
+		$filename = 'data.txt';	
 
-//Vérification des données reçues
-$data = json_decode($data_json);
-echo $data;
+		//Recupération des données json
+		$data_json = file_get_contents('php://input');
 
-if (!$data){
-	http_response_code(415);
-	exit();
-}
-elseif (! $data->temperature || ! $data->humidite){
-	http_response_code(400);
-	exit();
-}
+		//Vérification des données reçues
+		$data = json_decode($data_json);
+		echo $data;
 
-//Ecriture dans le fichier data.txt
-$op = file_put_contents($filename, $data_json);
-//Verif écriture
-if (! $op) {
-	echo "Store error" ;
-}
+		if (!$data){
+			http_response_code(415);
+			exit();
+		}
+		elseif (! $data->temperature || ! $data->humidite){
+			http_response_code(400);
+			exit();
+		}
 
-//Ecriture en bdd
-require_once 'pdoconfig.php';
-try {
-	//Connexion
-	$bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-	
-	//Recupération de la date de MAJ
-	$datetime = filemtime('store_temp.php');
+		//Ecriture dans le fichier data.txt
+		$op = file_put_contents($filename, $data_json);
+		//Verif écriture
+		if (! $op) {
+			echo "Store error" ;
+		}
 
-	//Préparation de la requête
-	$req = $bdd->prepare('INSERT INTO releves_dht11 (datetime, temperature, humidite)'.'VALUES (:datetime, :temperature, :humidite)');
-	
-	//Requête SQL
-	$req->execute(array(
-		'datetime' => $datetime,
-		'temperature' => $data->temperature,
-		'humidite' => $data->humidite
-		));
-	//Affichage du résultat
-	echo('<div>Un nouveau relevé a été ajouté à  : Température '.$data->temperature.'°C - Humidité '.$data->humidite.'%</div>');
+		//Ecriture en bdd
+		require_once 'pdoconfig.php';
+		try {
+			//Connexion
+			$bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-	$req = null;
+			//Recupération de la date de MAJ
+			$datetime = filemtime('store_temp.php');
 
-	$bdd = null;
-}
-catch (PDOException $e) {
-	print "Erreur : " . $e->getMessage() . "<br/>";
-	die();
-}
+			//Préparation de la requête
+			$req = $bdd->prepare('INSERT INTO releves_dht11 (datetime, temperature, humidite)'.'VALUES (:datetime, :temperature, :humidite)');
+
+			//Requête SQL
+			$req->execute(array(
+				'datetime' => $datetime,
+				'temperature' => $data->temperature,
+				'humidite' => $data->humidite
+				));
+			//Affichage du résultat
+			echo('<div>Un nouveau relevé a été ajouté à  : Température '.$data->temperature.'°C - Humidité '.$data->humidite.'%</div>');
+
+			$req = null;
+
+			$bdd = null;
+		}
+		catch (PDOException $e) {
+			print "Erreur : " . $e->getMessage() . "<br/>";
+			die();
+		} ?>
+    </body>
+</html>
